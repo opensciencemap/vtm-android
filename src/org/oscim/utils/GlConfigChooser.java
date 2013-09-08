57ce608d5d07f4263854b29025d9cd2d8c783fea
Hannes Janetzek <hannes.janetzek@gmail.com>
2013-09-08 16:27:47 +0200

    EGLConfing: do not explicitly request alpha channel - should fix issue with rgb565

--- src/org/oscim/utils/GlConfigChooser.java
+++ src/org/oscim/utils/GlConfigChooser.java
@@ -14,23 +14,18 @@ import android.util.Log;
 public class GlConfigChooser implements GLSurfaceView.EGLConfigChooser {
 	static private final String TAG = "ConfigChooser";
 
-	/**
-	 *
-	 */
-	public static int stencilSize = 0;
-
 	@Override
 	public EGLConfig chooseConfig(EGL10 egl, EGLDisplay display) {
 		mValue = new int[1];
 
 		// Try to find a normal multisample configuration first.
 		int[] configSpec = {
-				EGL10.EGL_RED_SIZE, 5,
-				EGL10.EGL_GREEN_SIZE, 6,
-				EGL10.EGL_BLUE_SIZE, 5,
-				EGL10.EGL_ALPHA_SIZE, 8,
+				// EGL10.EGL_RENDERABLE_TYPE, 4, EGL10.EGL_NONE };
+				EGL10.EGL_RED_SIZE, 8,
+				EGL10.EGL_GREEN_SIZE, 8,
+				EGL10.EGL_BLUE_SIZE, 8,
+				EGL10.EGL_ALPHA_SIZE, 0,
 				EGL10.EGL_DEPTH_SIZE, 16,
-				// Requires that setEGLContextClientVersion(2) is called on the view.
 				EGL10.EGL_RENDERABLE_TYPE, 4 /* EGL_OPENGL_ES2_BIT */,
 				EGL10.EGL_STENCIL_SIZE, 8,
 				EGL10.EGL_NONE };
@@ -38,71 +33,40 @@ public class GlConfigChooser implements GLSurfaceView.EGLConfigChooser {
 		if (!egl.eglChooseConfig(display, configSpec, null, 0, mValue)) {
 			throw new IllegalArgumentException("eglChooseConfig failed");
 		}
-		int numConfigs = mValue[0];
 
-		if (numConfigs <= 0) {
-			stencilSize = 4;
-
-			configSpec = new int[] {
-					// EGL10.EGL_RENDERABLE_TYPE, 4, EGL10.EGL_NONE };
-					EGL10.EGL_RED_SIZE, 8,
-					EGL10.EGL_GREEN_SIZE, 8,
-					EGL10.EGL_BLUE_SIZE, 8,
-					EGL10.EGL_ALPHA_SIZE, 8,
-					EGL10.EGL_DEPTH_SIZE, 16,
-					EGL10.EGL_RENDERABLE_TYPE, 4 /* EGL_OPENGL_ES2_BIT */,
-					EGL10.EGL_STENCIL_SIZE, 8,
-					EGL10.EGL_NONE };
+		if (mValue[0] <= 0) {
+			//EGL10.EGL_RED_SIZE, 5,
+			configSpec[1] = 5;
+			//EGL10.EGL_GREEN_SIZE, 6,
+			configSpec[3] = 6;
+			//EGL10.EGL_BLUE_SIZE, 5,
+			configSpec[5] = 5;
+			//EGL10.EGL_ALPHA_SIZE, 0,
+			//configSpec[7] = 0;
 
 			if (!egl.eglChooseConfig(display, configSpec, null, 0, mValue)) {
 				throw new IllegalArgumentException("eglChooseConfig failed");
 			}
-			numConfigs = mValue[0];
+		}
 
-			if (numConfigs <= 0) {
-				throw new IllegalArgumentException("No configs match configSpec");
-			}
-		} else {
-			stencilSize = 8;
+		int numConfigs = mValue[0];
+		if (numConfigs <= 0) {
+			throw new IllegalArgumentException("No configs match configSpec");
 		}
 
 		// Get all matching configurations.
 		EGLConfig[] configs = new EGLConfig[numConfigs];
 		if (!egl.eglChooseConfig(display, configSpec, configs, numConfigs, mValue)) {
-			throw new IllegalArgumentException("data eglChooseConfig failed");
+			throw new IllegalArgumentException("eglChooseConfig failed");
 		}
+
+		for (int i = 0; i < configs.length; ++i)
+			Log.i(TAG, printConfig(egl, display, configs[i]));
 
 		// CAUTION! eglChooseConfigs returns configs with higher bit depth
 		// first: Even though we asked for rgb565 configurations, rgb888
 		// configurations are considered to be "better" and returned first.
 		// You need to explicitly filter the data returned by eglChooseConfig!
-
-		// for (int i = 0; i < configs.length; ++i) {
-		// Log.i(TAG, printConfig(egl, display, configs[i]));
-		// }
-
-		// int index = -1;
-		// for (int i = 0; i < configs.length; ++i) {
-		// // if (findConfigAttrib(egl, display, configs[i], EGL10.EGL_RED_SIZE, 0) == 8
-		// // &&
-		// // findConfigAttrib(egl, display, configs[i], EGL10.EGL_ALPHA_SIZE, 0) == 0) {
-		// // index = i;
-		// // break;
-		// // }
-		// // else
-		// if (findConfigAttrib(egl, display, configs[i], EGL10.EGL_RED_SIZE, 0) == 8
-		// &&
-		// findConfigAttrib(egl, display, configs[i], EGL10.EGL_ALPHA_SIZE, 0) == 0
-		// &&
-		// findConfigAttrib(egl, display, configs[i], EGL10.EGL_DEPTH_SIZE, 0) == 24) {
-		// index = i;
-		// break;
-		// }
-		// }
-		// if (index == -1) {
-		// Log.w(TAG, "Did not find sane config, using first");
-		// index = 0;
-		// }
 		int index = 0;
 
 		Log.i(TAG, "using: " + printConfig(egl, display, configs[index]));
